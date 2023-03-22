95a84998322b40f6111a7dd210d0df7ed008236d
Patrick Tanta <43278282+PatrickTanta@users.noreply.github.com>
2023-03-22 09:42:03 -0500

    feat: custom item counter to handle cart quantity

--- components/ui/ItemCounter.tsx
+++ components/ui/ItemCounter.tsx
@@ -1,21 +1,35 @@
-import { FC } from "react"
-import { Box, IconButton, Typography } from "@mui/material"
-import { AddCircleOutline, RemoveCircleOutline } from "@mui/icons-material"
+import { FC } from 'react'
+import { Box, IconButton, Typography } from '@mui/material'
+import { AddCircleOutline, RemoveCircleOutline } from '@mui/icons-material'
 
 interface Props {
-
+    currentValue: number
+    maxValue: number
+    onUpdateQuantity: (currentValue: number) => void
 }
 
-export const ItemCounter: FC<Props> = () => {
-  return (
-    <Box display='flex' alignItems='center'>
-        <IconButton>
-            <RemoveCircleOutline />
-        </IconButton>
-        <Typography sx={{ width: 40, textAlign: 'center' }}>1</Typography>
-        <IconButton>
-            <AddCircleOutline />
-        </IconButton>
-    </Box>
-  )
+export const ItemCounter: FC<Props> = ({
+    currentValue,
+    onUpdateQuantity,
+    maxValue
+}) => {
+    return (
+        <Box display="flex" alignItems="center">
+            <IconButton
+                onClick={() => onUpdateQuantity(currentValue - 1)}
+                disabled={currentValue === 1}
+            >
+                <RemoveCircleOutline />
+            </IconButton>
+            <Typography sx={{ width: 40, textAlign: 'center' }}>
+                {currentValue}
+            </Typography>
+            <IconButton
+                onClick={() => onUpdateQuantity(currentValue + 1)}
+                disabled={currentValue === maxValue}
+            >
+                <AddCircleOutline />
+            </IconButton>
+        </Box>
+    )
 }
